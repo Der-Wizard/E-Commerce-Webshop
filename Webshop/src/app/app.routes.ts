da516749c39a549db695d10ae3d688d4ef4606ae
Nikita Korman <nikita.korman@foerch.de>
2024-09-19 11:53:17 +0200

    [fix]: routing

--- Webshop/src/app/app.routes.ts
+++ Webshop/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './auth.guard';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: HomeComponent, canActivate: [authGuard] },
+    { path:'products', component: HomeComponent, canActivate: [authGuard] },
+    { path:'about', component: AboutComponent, canActivate: [authGuard] }
+];
